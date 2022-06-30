7b148cb2f1736bc0f112944a15cd3c7d09e2d106
Tame <tameruh.s@gmail.com>
2022-06-30 15:37:39 +0300

    🛸 ci(packaging): War packaging implemented

--- src/main/java/com/softech/ehr/EhrApiApplication.java
+++ src/main/java/com/softech/ehr/EhrApiApplication.java
@@ -1,22 +1,17 @@
 package com.softech.ehr;
 
-import com.github.javafaker.Faker;
-import com.softech.ehr.dto.AzModelMapper;
-
-import org.mapstruct.factory.Mappers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EhrApiApplication {
+public class EhrApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EhrApiApplication.class, args);
     }
-
 
 
 }
