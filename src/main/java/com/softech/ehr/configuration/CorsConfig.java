1a3fea56e9a79d1e7ca543f8b0a6edb5ac7153bc
Tame <tameruh.s@gmail.com>
2022-07-17 11:10:47 +0300

    🐛 fix(Security): CORS

--- src/main/java/com/softech/ehr/configuration/CorsConfig.java
+++ src/main/java/com/softech/ehr/configuration/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
        @Override
        public void addCorsMappings(CorsRegistry registry) {
            registry.addMapping("/**")
-               .allowedOriginPatterns("*");
+               .allowedHeaders("*")
+               .allowedOrigins("*");
        }
    };
     }
